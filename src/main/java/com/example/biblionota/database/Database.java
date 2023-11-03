a69aa68f624e7be1b64a2b38aa08fd92a599724d
Sierra Riley <w0791053@myscc.ca>
2023-11-03 10:45:28 -0400

    created Database *need to adjust to login

--- src/main/java/com/example/biblionota/database/Database.java
+++ src/main/java/com/example/biblionota/database/Database.java
@@ -1,4 +1,7 @@
 package com.example.biblionota.database;
+import java.sql.*;
+
+import static com.example.biblionota.database.Const.*;
 
 public class Database {
     /**
@@ -8,4 +11,69 @@ public class Database {
      * The static method getInstance() will create/return the one instance of the database class
      * Allowing the application to only ever have one instance of the Database class
      */
+
+
+
+    private static Database instance;
+   private Connection connection = null;
+
+
+    /**
+     * Creates a connection to a Database
+     * Will also make tables in Database
+     */
+    private Database(){
+        try{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        //Fix this next line - base it off of login
+        connection = DriverManager.getConnection("jdbc:mysql://localhost/" + DB_NAME + "?serverTimezone=UTC", DB_USER, DB_PASS);
+            System.out.println("Created Connection");
+
+            //create test connection button
+            //createtables() <- make tables here
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+    /**
+     * This method will create an instance if one has not been created yet. If so, it will use the instance that has been created
+     * @return instance
+     */
+    public static Database getInstance(){
+        if(instance == null){
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    /**
+     * This method will create a table, if table is already created, it will tell you
+     * @param tableName
+     * @param tableQry
+     * @param connection
+     * @throws SQLException
+     */
+    private void createTable(String tableName, String tableQry, Connection connection) throws SQLException{
+        Statement createTable;
+        DatabaseMetaData metaData = connection.getMetaData();
+
+        //look in database for table that matches tableName param
+        ResultSet resultSet = metaData.getTables("srileymd", null, tableName, null);
+        if(resultSet.next()){
+            System.out.println(tableName + " table already exists");
+        }else{
+            createTable = connection.createStatement();
+            createTable.execute(tableQry);
+            System.out.println(tableName + " table has been created!");
+
+            //INSERT statements go here.
+        }
+    }
+
+    public Connection getConnection(){return connection;}
+
+
+
 }
