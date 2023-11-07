b638e9f468fdbab224c3c073c6a12f00973e10f5
Sage Lawrence <w0811501@myscc.ca>
2023-11-07 10:03:16 -0500

    #16 DBConst Page Set Up

--- src/main/java/com/example/biblionota/database/Const.java
+++ src/main/java/com/example/biblionota/database/Const.java
@@ -2,7 +2,7 @@ package com.example.biblionota.database;
 
 public class Const {
     //TODO: Alter these values to proper ones
-    public static final String DB_NAME = "usernamemd";
+    public static final String DB_NAME = "usernamejava";
     public static final String DB_USER = "username";
     public static final String DB_PASS = "longpassword";
 }
