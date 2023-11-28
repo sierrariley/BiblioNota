08deca855a08a5082ec5ec37d48d18468b7e8c19
Sage Lawrence <w0811501@myscc.ca>
2023-11-28 09:49:52 -0500

    Fixed Issue with deleteBook()

--- src/main/java/com/example/biblionota/tables/BookTable.java
+++ src/main/java/com/example/biblionota/tables/BookTable.java
@@ -126,11 +126,22 @@ public class BookTable implements BookDAO {
 
     @Override
     public void deleteBook(int id) {
-        String query = "DELETE FROM " + DBConst.TABLE_BOOK + " WHERE " +
+        //Delete from books_authors where id = xxx
+        String query1 = "DELETE FROM " + DBConst.TABLE_BOOK_AUTHOR + " WHERE " +
+                DBConst.BOOK_AUTHOR_COLUMN_ID + " = " + id;
+        //Delete from books_tags where id = xxx
+        String query2 = "DELETE FROM " + DBConst.TABLE_BOOK_TAGS + " WHERE " +
+                DBConst.BOOK_TAGS_COLUMN_ID + " = " + id;
+        //Delete from books where id = xxx
+        String query3 = "DELETE FROM " + DBConst.TABLE_BOOK + " WHERE " +
                 DBConst.BOOK_COLUMN_ID + " = " + id;
-        System.out.println(query);
+        System.out.println(query3);
         try {
-            db.getConnection().createStatement().execute(query);
+            db.getConnection().createStatement().execute(query1);
+            System.out.println("Deleted books_authors instance");
+            db.getConnection().createStatement().execute(query2);
+            System.out.println("Deleted books_tags instance");
+            db.getConnection().createStatement().execute(query3);
             System.out.println("Deleted Record");
         } catch (SQLException e) {
             throw new RuntimeException(e);
