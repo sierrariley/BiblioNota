9d2ae1e818c4e3700107661a8588b140a5d37d20
Sierra Riley <w0791053@myscc.ca>
2023-12-04 21:56:04 -0500

    added another credit

--- src/main/java/com/example/biblionota/Panes/CreditsPane.java
+++ src/main/java/com/example/biblionota/Panes/CreditsPane.java
@@ -22,6 +22,7 @@ public class CreditsPane extends StackPane {
         HBox hBox1 = new HBox();
         HBox hBox2 = new HBox();
         HBox hBox3 = new HBox();
+        HBox hBox4 = new HBox();
 
         Label authorTitle = new Label("Authors: ");
         Label authors = new Label("Sage Lawrence & Sierra Riley");
@@ -32,20 +33,24 @@ public class CreditsPane extends StackPane {
         Label assetsTitle = new Label("Assets Acquired: ");
         Label assets = new Label("Assets created using Canva.");
 
+        Label freepik = new Label("Background Image from Freepik.com");
+
         authorTitle.getStyleClass().add("label");
         authors.getStyleClass().add("label");
         licenseTitle.getStyleClass().add("label");
         license.getStyleClass().add("label");
         assetsTitle.getStyleClass().add("label");
         assets.getStyleClass().add("label");
+        freepik.getStyleClass().add("label");
         vBox.getStyleClass().add("root");
 
 
         hBox1.getChildren().addAll(authorTitle, authors);
         hBox2.getChildren().addAll(licenseTitle, license);
         hBox3.getChildren().addAll(assetsTitle, assets);
+        hBox4.getChildren().add(freepik);
 
-        vBox.getChildren().addAll(hBox1, hBox2, hBox3);
+        vBox.getChildren().addAll(hBox1, hBox2, hBox3, hBox4);
         vBox.setAlignment(Pos.CENTER);
         vBox.setSpacing(10);
         vBox.setPadding(new Insets(0, 0, 0, 200));
